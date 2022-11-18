3f52d729e4a8bf08aeaedd0ff66582d7c99e7414
Filipe <fgallo_dev@hotmail.com>
2022-11-18 16:11:56 -0300

    add header in the function return

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header/Header";
 
 export default function Home() {
   return (
@@ -12,7 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <Header />
+
+      <main>Tela inicial</main>
 
       <footer></footer>
     </div>
