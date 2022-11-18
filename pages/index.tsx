75e7e873ffe7f6ced216f2d0823500d5de0ae6b8
Filipe <fgallo_dev@hotmail.com>
2022-11-18 13:31:27 -0300

    First commit / add first project files

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Filipe Gallo - Desenvolvedor Front-End</title>
+        <meta
+          name="description"
+          content="Site oficial do Desenvolvedor Front-End Filipe Gallo com portfólio de projetos e outras informações."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
