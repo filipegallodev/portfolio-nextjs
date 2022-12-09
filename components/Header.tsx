46c056a58b52d496f14bb68ada77f6188c44de18
Filipe <filipegallodev@hotmail.com>
2022-12-09 11:37:09 -0300

    feat: theme handler

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,6 +30,74 @@ const Header = () => {
   const router = useRouter();
   const currentRoute = router.pathname;
 
+  const [theme, setTheme] = React.useState("");
+  const [themeImage, setThemeImage] = React.useState(
+    "/assets/themes-icons/dark.png"
+  );
+
+  React.useEffect(() => {
+    const savedTheme = localStorage.getItem("user-theme");
+
+    if (savedTheme === "light") {
+      return setLightTheme();
+    }
+
+    localStorage.setItem("user-theme", "dark");
+    setDarkTheme();
+  }, []);
+
+  React.useEffect(() => {
+    if (theme) {
+      localStorage.setItem("user-theme", theme);
+    }
+  }, [theme]);
+
+  function changeTheme() {
+    const savedTheme = getSavedTheme();
+
+    if (savedTheme === "dark") {
+      setLightTheme();
+    }
+
+    if (savedTheme === "light") {
+      setDarkTheme();
+    }
+  }
+
+  function getSavedTheme() {
+    return localStorage.getItem("user-theme");
+  }
+
+  function setLightTheme() {
+    const docStyle = document.documentElement.style;
+
+    docStyle.setProperty("--main-text-color", "#050505");
+
+    docStyle.setProperty("--main-background-color", "#f5f5f5");
+
+    docStyle.setProperty("--card-text-color", "#222");
+
+    docStyle.setProperty("--card-background-color", "#ccc");
+
+    setThemeImage("/assets/themes-icons/light.png");
+    setTheme("light");
+  }
+
+  function setDarkTheme() {
+    const docStyle = document.documentElement.style;
+
+    docStyle.setProperty("--main-text-color", "#f5f5f5");
+
+    docStyle.setProperty("--main-background-color", "#050505");
+
+    docStyle.setProperty("--card-text-color", "#f5f5f5");
+
+    docStyle.setProperty("--card-background-color", "#252525");
+
+    setThemeImage("/assets/themes-icons/dark.png");
+    setTheme("dark");
+  }
+
   return (
     <header>
       <div className={styles.header}>
@@ -51,9 +119,15 @@ const Header = () => {
             ))}
           </ul>
         </nav>
-        <div>
-          <label htmlFor="theme">Tema</label>
-          <input type="checkbox" name="theme" id="theme" />
+        <div className={styles.themeImage}>
+          <span>Tema</span>
+          <Image
+            onClick={changeTheme}
+            src={themeImage}
+            alt="Tema"
+            width={48}
+            height={48}
+          />
         </div>
       </div>
     </header>
