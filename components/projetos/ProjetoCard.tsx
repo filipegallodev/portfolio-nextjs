85e4fd6a761429dee66472af71a3e8d00377af0c
Filipe <filipegallodev@hotmail.com>
2023-01-05 09:58:46 -0300

    fix: project interface on useState and removed unused variables

--- components/projetos/ProjetoCard.tsx
+++ components/projetos/ProjetoCard.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import React from "react";
+import ProjetoInfo from "./ProjetoInfo";
+import { Projeto } from "./ProjetoInfo";
 
 import styles from "../../styles/projetocard.module.css";
-import ProjetoInfo from "./ProjetoInfo";
 
 const ProjetoCard = () => {
-  const [projeto, setProjeto] = React.useState<any>();
+  const [projeto, setProjeto] = React.useState<Projeto | null>();
   const [listaProjetos, setListaProjetos] = React.useState([]);
 
   React.useEffect(() => {
@@ -30,17 +31,13 @@ const ProjetoCard = () => {
     setProjeto(projetoProps[0]);
   }
 
-  function previousButton() {
-    setProjeto(null);
-  }
-
   return (
     <React.Fragment>
       {projeto ? (
         <ProjetoInfo setProjeto={setProjeto} projeto={projeto} />
       ) : (
         <div className={styles.projetosContainer}>
-          {listaProjetos.map(({ id, name, description, imageUrl }) => (
+          {listaProjetos.map(({ id, name, imageUrl }) => (
             <div
               className={styles.projetoCard + " animeLeft"}
               onClick={handleClick}
--- components/projetos/ProjetoInfo.tsx
+++ components/projetos/ProjetoInfo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import styles from "../../styles/projetoinfo.module.css";
 
-interface Projeto {
+export interface Projeto {
   name: string;
   id: string;
   description: string;
